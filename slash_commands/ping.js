37e84272506290e10f185df42178b732ada26e09
arin <eyeamarinsharma@gmail.com>
2023-05-16 15:15:29 +0530

    created the ping command the ping.js file next step would be to load the file into the index.js

--- slash_commands/ping.js
+++ slash_commands/ping.js
@@ -1,4 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
-const command = new SlashCommandBuilder()
 
-console.log()
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	},
+};
