fce9f59cb81b615570edcbd581b249fa18d02b8e
arin <eyeamarinsharma@gmail.com>
2023-05-18 20:38:35 +0530

    updated the ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-
-
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+		.setName('ban')
+		.setDescription('Select a member and ban them.')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('The member to ban')
+				.setRequired(true))
+		.addStringOption(option =>
+			option
+				.setName('reason')
+				.setDescription('The reason for banning'))
+		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+		.setDMPermission(false),
+		async execute(interaction) {
+			const target = interaction.options.getUser('target');
+			const reason = interaction.options.getString('reason') ?? 'No reason provided';
+	
+			await interaction.reply(`Banning ${target.username} for reason: ${reason}`);
+			await interaction.guild.members.ban(target);
+		},
 };
+
+
